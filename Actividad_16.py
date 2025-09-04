abd3896af808c962816eb30ef18398d0e5950afb
tomas <tomas27@gmail.com>
2025-09-04 14:39:24 -0600

    2

--- Actividad_16.py
+++ Actividad_16.py
@@ -1,1 +1,25 @@
-print("Actividad 16")
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title("Calculadora ;)")
+ventana.geometry("300x300")
+
+etiqueta1 = tk.Label(ventana, text="Escribe el primer número: ")
+etiqueta1.pack(pady=5)
+entrada1= tk.Entry(ventana)
+entrada1.pack(pady=5)
+etiqueta2 = tk.Label(ventana, text="Escribe el segundo número: ")
+etiqueta2.pack(pady=5)
+entrada2 = tk.Entry(ventana)
+entrada2.pack(pady=5)
+
+resultado = tk.Label(ventana, text="Resultado: ")
+resultado.pack(pady=10)
+
+def Sumar():
+    try:
+        num1 = float(entrada1.get())
+        num2 = float(entrada2.get())
+        resultado.config(text=f"Resultado:{num1+num2} ")
+    except ValueError:
+        resultado.config(text="Error... Ingresa número validos")
